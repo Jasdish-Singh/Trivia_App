09096d6d2b440ae969b78170c10ce260477b0f75
Jasdish-Singh <jasdishsingh55@gmail.com>
2024-11-13 18:42:50 -0500

    first commit

--- Trivia_App/src/App.jsx
+++ Trivia_App/src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+ 
+
 
 function App() {
+    return (
+        <BrowserRouter>
+          <Routes>
+          </Routes>
+        </BrowserRouter>
+      );
 }
 
 export default App
